9af0af73b74e85a985dcea432e569d04816dd3fa
unknown <yy.ainu@gmail.com>
2020-10-14 21:36:41 +0300

    odigo burger-menu

--- odigo/scripts/header.js
+++ odigo/scripts/header.js
@@ -1,6 +1,6 @@
+// changing header
 (function () {
   const header = document.querySelector('.header')
-  const headerMenu = document.querySelector('.header__menu')
 
   window.onscroll = () => {
     if (window.pageYOffset > 30) {
@@ -10,4 +10,26 @@
       header.classList.remove('header__active')
     }
   }
-}())
+}());
+
+
+// burger menu
+(function () {
+  const burger = document.querySelector('.header__burger')
+  const menu = document.querySelector('.header__tablet_menu')
+  const menuClose = document.querySelector('.tablet__menu_close')
+  const menuItem = document.querySelectorAll('.tablet__menu_item')
+
+  burger.addEventListener('click', () => {
+    menu.style.transform = "translateY(0)"
+  })
+
+  menuClose.addEventListener('click', () => {
+    menu.style.transform = "translateY(-100%)"
+  })
+
+  menuItem.forEach(item => {
+    item.addEventListener('click', () => {
+      menu.style.transform = "translateY(-100%)"
+    })})
+}());
